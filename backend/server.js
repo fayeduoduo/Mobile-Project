afdcbf7a7072a54770b4940e1bb20fc737fa9894
fayeduoduo <fayeduoduo@gmail.com>
2023-02-09 23:21:44 -0800

    edit

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     //use get method('*') => match any route path can get response
     app.get('*', (req, res) => { 
-        res.sendFile(path.resolve(__dirname, "frontend","build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "app","frontend","build", "index.html"))
     })
 } else { 
     app.get('/', (req, res) => { 
