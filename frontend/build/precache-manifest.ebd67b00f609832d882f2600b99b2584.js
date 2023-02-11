eade2d86d57590f96475f402cfa8172df93ec98b
fayeduoduo <fayeduoduo@gmail.com>
2023-02-10 20:15:06 -0800

    edit

---
+++ frontend/build/precache-manifest.ebd67b00f609832d882f2600b99b2584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87764a23b46f5315bf0",
+    "url": "/static/css/main.278ff9a5.chunk.css"
+  },
+  {
+    "revision": "d87764a23b46f5315bf0",
+    "url": "/static/js/main.d87764a2.chunk.js"
+  },
+  {
+    "revision": "78dc5a2bb90860128d65",
+    "url": "/static/js/1.78dc5a2b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8a7b8ba74cf2225f260ddc780aba64d",
+    "url": "/index.html"
+  }
+];
